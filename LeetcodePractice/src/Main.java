8a772f2ad977922d4ec47e5d9f58c7406f84b4af
spawn050 <omkarsatam050@gmail.com>
2022-12-22 23:03:06 +0530

    Solved 88. Merge Sorted Array with inplace sorting(Inefficient method,0(n)=n^2

--- LeetcodePractice/src/Main.java
+++ LeetcodePractice/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {2,0,2,1,1,0};
-        SortColors.sortColors(array);
+        int[] array1 = {1,2,3,0,0,0};
+        int[] array2 = {2,5,6};
+        int m = 2;
+        int n = 3;
+
+        MergeSortedArrays.merge(array1, 3, array2, n);
     }
 }
